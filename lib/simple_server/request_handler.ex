d9f54dcd48a682a627005d7527683cc260a97cfb
Todd Pickell <tapickell@gmail.com>
2019-06-16 19:34:09 -0500

    now that it works going back through the list to implement routes and endpoints

--- lib/simple_server/request_handler.ex
+++ lib/simple_server/request_handler.ex
@@ -1,28 +1,43 @@
 defmodule SimpleServer.RequestHandler do
 
+  defmodule SimpleServer.Static do
+    def hello_world(), do: {:ok, "Hello World!"}
+  end
+  alias SimpleServer.Static
+  alias FileService.Document
+
+
   require Logger
+
+  @text_type "text/plain"
+  @json_type "application/json"
 
   def process_request(%{action: action, path: path} = conn) do
     Logger.warn("REQUEST:=> #{inspect(conn)}")
     route(path, action, conn)
   end
 
-  defp route('/helloworld' = path, :GET = action, conn) do
-    Logger.warn("Route for #{path} : #{action} called")
-    {:ok, build_respponse({200, "OK", "Hello World!"})}
+  defp route('/hello_world', :GET, conn) do
+    {:ok, response} = Static.hello_world()
+    {:ok, build_respponse({200, "OK", @text_type, response})}
+  end
+
+  defp route('/documents', :POST, conn) do
+    {:ok, response} = Document.store(conn.data)
+    {:ok, build_respponse({201, "Created", @text_type, response})}
   end
 
   defp route(path, action, conn) do
-    Logger.warn("Route for #{path} : #{action} called")
-    {:ok, build_respponse({404, "NOT FOUND", ""})}
+    Logger.warn("404 :: Route for #{path} : #{action} called")
+    {:ok, build_respponse({404, "NOT FOUND", @json_type, ""})}
   end
 
   # TODO this should be in a different module
-  defp build_respponse({code, message, body}) do
+  defp build_respponse({code, message, content_type, body}) do
     length = byte_size(body) + 1
     """
     HTTP/1.1 #{code} #{message}\r
-    Content-Type: application/json\r
+    Content-Type: #{content_type}\r
     Content-Length: #{length}\r
     Date: #{DateTime.utc_now() |> DateTime.to_iso8601()}\r
     Access-Control-Allow-Origin: *\r
--- test/simple_server_test.exs
+++ test/simple_server_test.exs
@@ -6,34 +6,30 @@ defmodule SimpleServerTest do
   @test_json '{\"data\": \"Hello World\"}'
   @c_type "content-type"
   @json_type "application/json"
+  @text_type "text/plain"
 
-  defmodule TestServer do
+  defmodule ServerClient do
     use Tesla
     plug Tesla.Middleware.BaseUrl, "http://localhost:4040"
 
     def hello_world() do
-      get("/helloworld")
+      get("/hello_world")
     end
 
     def post(data) do
       post("/post", data, headers: [{@c_type, @json_type}])
     end
-
-  end
-
-  setup_all do
-    # Application.stop(:simple_server)
-    # :ok = Application.start(:simple_server)
-    :ok
   end
 
   test "GET /hello_world request" do
-    {:ok, response} = TestServer.hello_world()
-    assert response == []
+    {:ok, response} = ServerClient.hello_world()
+    assert response.status == 200
+    assert response.body == "Hello World!\n"
+    assert List.keyfind(response.headers, @c_type, 0) == {@c_type, @text_type}
   end
 
   test "JSON POST request with data" do
-    {:ok, response} = TestServer.post(@test_json)
+    {:ok, response} = ServerClient.post(@test_json)
     assert response.status == 404
     assert List.keyfind(response.headers, @c_type, 0) == {@c_type, @json_type}
   end
